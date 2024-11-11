26bc2f4360cda4097fc29d1988ccdb5f722b90cf
Umar Adejoh <stradox@gmail.com>
2024-11-11 20:49:51 +0100

    waypoint, move to Kubuntu

--- src/main/java/pro/arcodeh/collation_server/Application.java
+++ src/main/java/pro/arcodeh/collation_server/Application.java
@@ -3,15 +3,14 @@ package pro.arcodeh.collation_server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import pro.arcodeh.collation_server.seeder.PollingUnitSeeder;
 
 @SpringBootApplication
-public class CollationServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CollationServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
---
+++ src/test/java/pro/arcodeh/collation_server/election/ElectionServiceTest.java
@@ -0,0 +1,16 @@
+package pro.arcodeh.collation_server.election;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElectionServiceTest {
+    ElectionService electionService;
+
+    @BeforeEach
+    void setUp() {
+        electionService = new ElectionService();
+
+
+    }
+}
